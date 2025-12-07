30e149d754a8aa0a48973f621b322a65c721ef6c
EmanuelMarquessf <emanuel.marquessf@gmail.com>
2025-12-07 20:45:12 -0300

    feat: atualizando header e footer, corrigindo categorias

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const vinhosJson = [
     {
         id: 1,
         title: "Natural Delama Brut",
-        type: "Espumante",
+        type: "Vinho Espumante",
         description: `
           Elaborado pelo método charmat, apresenta elegante perlage, fineza aromática, boa cremosidade, excelente equilíbrio e persistência.
         `,
@@ -12,7 +12,7 @@ export const vinhosJson = [
         ],
         topics: "Produção limitada (lotes pequenos e controlados); Foco na qualidade, não na quantidade; Uvas rigorosamente selecionadas e vinificadas em pequenas autoclaves; Enólogo promissor; Região e vinhedo: Garibaldi (melhores espumantes do Brasil); Público-alvo: hotéis, restaurantes e lojas especializadas; Exclusividade comercial (não competimos com lojas online de massa); Linha de elaboração própria.",
         harmonization: "A região escolhida foi Garibaldi, reconhecida por seu terroir e excelência na produção de espumantes premium. Unimos essa tradição à nossa filosofia de valorizar enólogos talentosos — ainda pouco conhecidos, mas com enorme dedicação, técnica e paixão pelo que fazem — garantindo que cada detalhe, do vinhedo à taça, seja tratado com o máximo cuidado.",
-        image: "./public/img/garrafas/Espumantes/2.png",
+        image: "./public/img/garrafas/Espumantes/delama-brut.png",
         url: "https://www.botecodovinho.com.br/produto/delama-espumante-natural-brut-750ml/",
         grape: "Chardonnay, Riesling Itálico e Prosecco",
         country: "Brasil",
@@ -24,64 +24,52 @@ export const vinhosJson = [
     },
     {
         id: 2,
-        title: "Natural Delama Brut",
-        type: "Vinho Suave",
-        description: `
-          Elaborado pelo método charmat, apresenta elegante perlage, fineza aromática, boa cremosidade, excelente equilíbrio e persistência.
-        `,
-        detailDescription:[
-            "Iniciado em 2024, este projeto nasceu da seleção criteriosa das melhores uvas para a criação dos novos espumantes Arturo Vinhos. O lançamento está previsto para o final de 2025 — um brinde especial a mais um passo importante da nossa história.",
-            "A região escolhida foi Garibaldi, reconhecida por seu terroir e excelência na produção de espumantes premium. Unimos essa tradição à nossa filosofia de valorizar enólogos talentosos — ainda pouco conhecidos, mas com enorme dedicação, técnica e paixão pelo que fazem — garantindo que cada detalhe, do vinhedo à taça, seja tratado com o máximo cuidado.",
-        ],
-        topics: "Produção limitada (lotes pequenos e controlados); Foco na qualidade, não na quantidade; Uvas rigorosamente selecionadas e vinificadas em pequenas autoclaves; Enólogo promissor; Região e vinhedo: Garibaldi (melhores espumantes do Brasil); Público-alvo: hotéis, restaurantes e lojas especializadas; Exclusividade comercial (não competimos com lojas online de massa); Linha de elaboração própria.",
-        harmonization: "A região escolhida foi Garibaldi, reconhecida por seu terroir e excelência na produção de espumantes premium. Unimos essa tradição à nossa filosofia de valorizar enólogos talentosos — ainda pouco conhecidos, mas com enorme dedicação, técnica e paixão pelo que fazem — garantindo que cada detalhe, do vinhedo à taça, seja tratado com o máximo cuidado.",
-        image: "./public/img/garrafas/Espumantes/2.png",
+        title: "Florada Dos Campos",
+        type: "Vinho Espumante",
+        description: ``,
+        detailDescription:[],
+        topics: "",
+        harmonization: "",
+        image: "./public/img/garrafas/Espumantes/florada-campos.png",
         url: "",
-        grape: "Chardonnay, Riesling Itálico e Prosecco",
-        country: "Brasil",
-        region: "Garibaldi",
-        productor: "Vinicola São Luis",
-        safra: "2025",
-        alcolic: "12.5%",
-        volume: "750ml"
+        grape: "",
+        country: "",
+        region: "",
+        productor: "",
+        safra: "",
+        alcolic: "",
+        volume: ""
     },
     {
         id: 3,
-        title: "Natural Delama Brut",
-        type: "Espumante",
-        description: `
-          Elaborado pelo método charmat, apresenta elegante perlage, fineza aromática, boa cremosidade, excelente equilíbrio e persistência.
-        `,
+        title: "Florada dos Campos Moscatel",
+        type: "Vinho Branco",
+        description: ``,
         detailDescription:[
-            "Iniciado em 2024, este projeto nasceu da seleção criteriosa das melhores uvas para a criação dos novos espumantes Arturo Vinhos. O lançamento está previsto para o final de 2025 — um brinde especial a mais um passo importante da nossa história.",
-            "A região escolhida foi Garibaldi, reconhecida por seu terroir e excelência na produção de espumantes premium. Unimos essa tradição à nossa filosofia de valorizar enólogos talentosos — ainda pouco conhecidos, mas com enorme dedicação, técnica e paixão pelo que fazem — garantindo que cada detalhe, do vinhedo à taça, seja tratado com o máximo cuidado.",
         ],
-        topics: "Produção limitada (lotes pequenos e controlados); Foco na qualidade, não na quantidade; Uvas rigorosamente selecionadas e vinificadas em pequenas autoclaves; Enólogo promissor; Região e vinhedo: Garibaldi (melhores espumantes do Brasil); Público-alvo: hotéis, restaurantes e lojas especializadas; Exclusividade comercial (não competimos com lojas online de massa); Linha de elaboração própria.",
-        harmonization: "A região escolhida foi Garibaldi, reconhecida por seu terroir e excelência na produção de espumantes premium. Unimos essa tradição à nossa filosofia de valorizar enólogos talentosos — ainda pouco conhecidos, mas com enorme dedicação, técnica e paixão pelo que fazem — garantindo que cada detalhe, do vinhedo à taça, seja tratado com o máximo cuidado.",
-        image: "./public/img/garrafas/Espumantes/2.png",
+        topics: "",
+        harmonization: "",
+        image: "./public/img/garrafas/Vinhos Brancos/moscatel.png",
         url: "",
-        grape: "Chardonnay, Riesling Itálico e Prosecco",
-        country: "Brasil",
-        region: "Garibaldi",
-        productor: "Vinicola São Luis",
-        safra: "2025",
+        grape: "",
+        country: "",
+        region: "",
+        productor: "",
+        safra: "",
         alcolic: "12.5%",
-        volume: "750ml"
+        volume: ""
     },
     {
         id: 4,
-        title: "Natural Delama Brut",
-        type: "Espumante",
+        title: "Delaso",
+        type: "Vinho Suave",
         description: `
-          Elaborado pelo método charmat, apresenta elegante perlage, fineza aromática, boa cremosidade, excelente equilíbrio e persistência.
         `,
         detailDescription:[
-            "Iniciado em 2024, este projeto nasceu da seleção criteriosa das melhores uvas para a criação dos novos espumantes Arturo Vinhos. O lançamento está previsto para o final de 2025 — um brinde especial a mais um passo importante da nossa história.",
-            "A região escolhida foi Garibaldi, reconhecida por seu terroir e excelência na produção de espumantes premium. Unimos essa tradição à nossa filosofia de valorizar enólogos talentosos — ainda pouco conhecidos, mas com enorme dedicação, técnica e paixão pelo que fazem — garantindo que cada detalhe, do vinhedo à taça, seja tratado com o máximo cuidado.",
         ],
-        topics: "Produção limitada (lotes pequenos e controlados); Foco na qualidade, não na quantidade; Uvas rigorosamente selecionadas e vinificadas em pequenas autoclaves; Enólogo promissor; Região e vinhedo: Garibaldi (melhores espumantes do Brasil); Público-alvo: hotéis, restaurantes e lojas especializadas; Exclusividade comercial (não competimos com lojas online de massa); Linha de elaboração própria.",
-        harmonization: "A região escolhida foi Garibaldi, reconhecida por seu terroir e excelência na produção de espumantes premium. Unimos essa tradição à nossa filosofia de valorizar enólogos talentosos — ainda pouco conhecidos, mas com enorme dedicação, técnica e paixão pelo que fazem — garantindo que cada detalhe, do vinhedo à taça, seja tratado com o máximo cuidado.",
-        image: "./public/img/garrafas/Espumantes/2.png",
+        topics: "",
+        harmonization: "",
+        image: "./public/img/garrafas/Vinhos Suaves/delaso.png",
         url: "",
         grape: "Chardonnay, Riesling Itálico e Prosecco",
         country: "Brasil",
