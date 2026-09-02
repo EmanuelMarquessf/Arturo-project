6d6c2c989aeb8ca65891e796c326e3f8398a7582
Emanuel Fonseca <emanuel.marquessf@gmail.com>
2026-09-01 22:24:48 -0300

    historia

--- src/js/historyPage.js
+++ src/js/historyPage.js
@@ -1,2 +1,27 @@
 import { setupHeaderInteractions } from "../components/header/header.js";
 import { setupFooterInteractions } from "../components/footer/footer.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  setupHeaderInteractions();
+  setupFooterInteractions();
+
+  const btnContinuar = document.getElementById("btn-continuar-leitura");
+  const iconeChevron = document.getElementById("icone-chevron");
+  const conteudoHistoria = document.getElementById("myHistoryExpanded");
+  const textoBotao = document.getElementById("texto-botao");
+  
+
+  if (btnContinuar && iconeChevron && conteudoHistoria) {
+    btnContinuar.addEventListener("click", () => {
+      btnContinuar.classList.toggle("col-span-4");
+      iconeChevron.classList.toggle("rotate-180");
+      conteudoHistoria.classList.toggle("hidden");
+
+      if (conteudoHistoria.classList.contains("hidden")) {
+        textoBotao.textContent = "Continuar Leitura";
+      } else {
+        textoBotao.textContent = "Ocultar Leitura";
+      }
+    });
+  }
+});
